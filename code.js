ba7fab59deecc94a7a2b3b62b6e436df181f1ac4
JasonScholte <56888977+JasonScholte@users.noreply.github.com>
2020-07-16 13:19:35 -0500

    still working

--- code.js
+++ code.js
@@ -8,8 +8,9 @@
   Return the number 5 from this function.
 */
 function getNumberFive() {
-  // Your code here
+ return 5;
 }
+console.log(getNumberFive());
 
 /* 
   -------TESTS---------------------------------------------------------------
@@ -27,7 +28,7 @@ console.log(getNumberFive() === 5);
   Multiply these two numbers and return the result.
 */
 function multiply(a, b) {
-  // You code here
+  return a * b;
 }
 
 /* 
@@ -50,7 +51,7 @@ console.log(multiply(0, 0) === 0);
   Add the text to the end of the message, and then return the complete string.
 */
 function concatenate(message, text) {
-  // You code here
+  return message + text;
 }
 
 /* 
@@ -73,7 +74,10 @@ console.log(concatenate("", "New text!") === "New text!");
   Otherwise, multiply the number by -1 and return it. 
 */
 function absoluteValue(number) {
-  // You code here
+  if (number > 0) {
+    return 5;
+  }
+  else { }
 }
 
 /* 
@@ -104,6 +108,7 @@ console.log(absoluteValue(0) === 0);
   (Look at the lesson on Operators)
 */
 function evenOddOrZero(number) {
+  if (number )
   // You code here
 }
 
