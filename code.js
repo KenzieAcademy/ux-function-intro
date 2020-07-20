8fc67392b6f2ea12ca34697b3a85c2a05645df94
April Chilldres <april.chilldres@gmail.com>
2020-07-20 03:10:54 -0500

    first commit

--- code.js
+++ code.js
@@ -8,15 +8,16 @@
   Return the number 5 from this function.
 */
 function getNumberFive() {
-  // Your code here
+	// Your code here
+	return 5;
 }
 
 /* 
   -------TESTS---------------------------------------------------------------
   Run these commands to make sure you did it right. They should all be true.
 */
-console.log("-----Tests for Exercise One-----");
-console.log("* Result is 5");
+console.log('-----Tests for Exercise One-----');
+console.log('* Result is 5');
 console.log(getNumberFive() === 5);
 
 /* ------------------------------------------------
@@ -27,19 +28,20 @@ console.log(getNumberFive() === 5);
   Multiply these two numbers and return the result.
 */
 function multiply(a, b) {
-  // You code here
+	// You code here
+	return a * b;
 }
 
 /* 
   -------TESTS---------------------------------------------------------------
   Run these commands to make sure you did it right. They should all be true.
 */
-console.log("-----Tests for Exercise Two-----");
-console.log("* Check for 2 and 3");
+console.log('-----Tests for Exercise Two-----');
+console.log('* Check for 2 and 3');
 console.log(multiply(2, 3) === 6);
-console.log("* Check for 5 and 6");
+console.log('* Check for 5 and 6');
 console.log(multiply(5, 6) === 30);
-console.log("* Check for 0 and 0");
+console.log('* Check for 0 and 0');
 console.log(multiply(0, 0) === 0);
 
 /* ------------------------------------------------
@@ -50,18 +52,21 @@ console.log(multiply(0, 0) === 0);
   Add the text to the end of the message, and then return the complete string.
 */
 function concatenate(message, text) {
-  // You code here
+	// You code here
+	const str1 = 'Hello';
+	const str2 = 'World';
+	console.log(str1.concat(' ', str2));
 }
 
 /* 
   -------TESTS---------------------------------------------------------------
   Run these commands to make sure you did it right. They should all be true.
 */
-console.log("-----Tests for Exercise Three-----");
-console.log("* Check basic message");
-console.log(concatenate("This is a ", "string!") === "This is a string!");
-console.log("* Check empty initial message");
-console.log(concatenate("", "New text!") === "New text!");
+console.log('-----Tests for Exercise Three-----');
+console.log('* Check basic message');
+console.log(concatenate('This is a ', 'string!') === 'This is a string!');
+console.log('* Check empty initial message');
+console.log(concatenate('', 'New text!') === 'New text!');
 
 /* ------------------------------------------------
   Exercise Four - absoluteValue
@@ -74,18 +79,27 @@ console.log(concatenate("", "New text!") === "New text!");
 */
 function absoluteValue(number) {
   // You code here
-}
+  if (number === 0); {
+    return "5";
+  }
+  if (number === 3) {
+    return "-3";
+  }
+  else  {
+    return "0";
+  }
+
 
 /* 
   -------TESTS---------------------------------------------------------------
   Run these commands to make sure you did it right. They should all be true.
 */
-console.log("-----Tests for Exercise Four-----");
-console.log("* Check for 5");
+console.log('-----Tests for Exercise Four-----');
+console.log('* Check for 5');
 console.log(absoluteValue(5) === 5);
-console.log("* Check for -3");
+console.log('* Check for -3');
 console.log(absoluteValue(-3) === 3);
-console.log("* Check for 0");
+console.log('* Check for 0');
 console.log(absoluteValue(0) === 0);
 
 /* ------------------------------------------------
@@ -97,7 +111,7 @@ console.log(absoluteValue(0) === 0);
   
   If the number is even, return "Even"
   If the number is odd, return "Odd"
-  If thenumber is zero, then return "Zero"
+  If the number is zero, then return "Zero"
 
   Hint: Remember the Modulo operator! % 
   How can you use it to determine if the number is odd or even? 
@@ -105,16 +119,34 @@ console.log(absoluteValue(0) === 0);
 */
 function evenOddOrZero(number) {
   // You code here
-}
+
+  for (var x=0; x<=15; x++) {
+    if (x === 0) {
+            console.log(x +  " is even");
+    }
+    else if (x % 2 === 0) {
+            console.log(x + " is even");   
+    }
+    else {
+            console.log(x + " is odd");
+    }
+
+
+
+
+
+
+
+  
 
 /* 
   -------TESTS---------------------------------------------------------------
   Run these commands to make sure you did it right. They should all be true.
 */
-console.log("-----Tests for Exercise Five-----");
-console.log("* Check for Even");
-console.log(evenOddOrZero(6) === "Even");
-console.log("* Check for Odd");
-console.log(evenOddOrZero(13) === "Odd");
-console.log("* Check for Zero");
-console.log(evenOddOrZero(0) === "Zero");
+console.log('-----Tests for Exercise Five-----');
+console.log('* Check for Even');
+console.log(evenOddOrZero(6) === 'Even');
+console.log('* Check for Odd');
+console.log(evenOddOrZero(13) === 'Odd');
+console.log('* Check for Zero');
+console.log(evenOddOrZero(0) === 'Zero');
