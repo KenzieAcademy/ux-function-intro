51d97dac1d5438d6ea36f37245c7cc8926840973
ssawyer0406 <ssawyer0406@gmail.com>
2020-07-23 02:15:04 -0500

    functions completed

--- code.js
+++ code.js
@@ -8,7 +8,7 @@
   Return the number 5 from this function.
 */
 function getNumberFive() {
-  // Your code here
+  return 5;
 }
 
 /* 
@@ -27,7 +27,7 @@ console.log(getNumberFive() === 5);
   Multiply these two numbers and return the result.
 */
 function multiply(a, b) {
-  // You code here
+  return a * b;
 }
 
 /* 
@@ -50,7 +50,7 @@ console.log(multiply(0, 0) === 0);
   Add the text to the end of the message, and then return the complete string.
 */
 function concatenate(message, text) {
-  // You code here
+  return message + text;
 }
 
 /* 
@@ -73,7 +73,13 @@ console.log(concatenate("", "New text!") === "New text!");
   Otherwise, multiply the number by -1 and return it. 
 */
 function absoluteValue(number) {
-  // You code here
+  let total;
+  if (number > 0) {
+    total = number;
+  } else {
+    total = number * -1;
+  }
+  return total;
 }
 
 /* 
@@ -104,7 +110,15 @@ console.log(absoluteValue(0) === 0);
   (Look at the lesson on Operators)
 */
 function evenOddOrZero(number) {
-  // You code here
+  let result;
+  if (number == 0) {
+    result = "Zero";
+  } else if (number % 2 == 0) {
+    result = "Even";
+  } else {
+    result = "Odd";
+  }
+  return result;
 }
 
 /* 
